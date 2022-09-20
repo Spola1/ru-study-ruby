cbf98065b7817365d17ef2e17821d212391ea0f2
Spola1 <1111.22224@mail.ru>
2022-09-20 18:18:49 +0300

    fp

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,11 +5,14 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(_array)
-        0
+        films = _array.select { |film| !film['country'].nil? && film['country'].split(',').length >= 2 && film['rating_kinopoisk'].to_f.positive? }
+        rating = films.reduce(0) { |total, film| total += film['rating_kinopoisk'].to_f }
+        rating / films.length
       end
 
       def chars_count(_films, _threshold)
-        0
+        films_rait_sel = _films.select { |film| film['rating_kinopoisk'].to_f >= _threshold }
+        films_rait_sel.map { |film| film['name'].count('и') }.reduce(:+)
       end
     end
   end
